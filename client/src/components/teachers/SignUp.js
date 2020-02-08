02e0d8c095fd81939855ad245467a285901c553c
Adam Sarli <adam.sarli@gmail.com>
2020-02-08 18:51:48 -0500

    initial setup of first action creator: signupTeacher

--- client/src/components/teachers/SignUp.js
+++ client/src/components/teachers/SignUp.js
@@ -1,12 +1,19 @@
 import React, { useState } from 'react'
 import './signup.css'
+import { connect } from 'react-redux'
+import { signupTeacher } from '../../actions/teacherActions.js'
 
-const SignUp = () => {
+const SignUp = ({ signupTeacher }) => {
   const [ firstName, setFirstName ] = useState('')
   const [ lastName, setLastName ] = useState('')
 
   const submitHandler = () => {
-    console.log(firstName, lastName)
+    const data = {
+      first_name: firstName,
+      last_name: lastName
+    }
+    
+    signupTeacher(data)
   }
 
   return (
@@ -36,4 +43,14 @@ const SignUp = () => {
   )
 }
 
-export default SignUp
+function mapStateToProps(state){
+  return {}
+}
+
+function mapDispatchToProps(dispatch){
+  return {
+    signupTeacher: (data) => dispatch(signupTeacher(data))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SignUp)
