2ad4039c33652a7029865a7def3b43ffa3362799
Adam Sarli <adam.sarli@gmail.com>
2020-02-08 17:47:44 -0500

    added <NavBar /> and react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import Login from './sessions/Login'
+import NavBar from './navBar/NavBar'
+import { Route, BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-      <main>
-        <h1>Seating Chart App!</h1>
-      </main>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <NavBar />
+        </header>
+        <main>
+          <Route exact path="/" component={Login} />
+        </main>
+      </div>
+    </Router>
   );
 }
 
