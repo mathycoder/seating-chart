f711339ed1ff6974471d8c7e2249dc2d1b1e5e3a
Adam Sarli <adam.sarli@gmail.com>
2020-02-09 21:11:19 -0500

    slight change for fallback_index_html

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get '*path', to: "static_pages#index", constraints: ->(request) do
+  get '*path', to: "application#fallback_index_html", constraints: ->(request) do
     !request.xhr? && request.format.html?
   end
-  
+
   get '/get_current_user', to: 'sessions#get_current_user'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
