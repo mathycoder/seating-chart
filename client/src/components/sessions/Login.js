6dda367bb82efc10bc475a6ca2b74c108e9616d1
Adam Sarli <adam.sarli@gmail.com>
2020-02-09 20:41:47 -0500

    slight change to Login form

--- client/src/components/sessions/Login.js
+++ client/src/components/sessions/Login.js
@@ -34,7 +34,7 @@ const Login = ({ login, history }) => {
         />
         <button
           onClick={() => submitHandler()}
-          className="myButton">Create Account
+          className="myButton">Log In
         </button>
       </div>
     </div>
