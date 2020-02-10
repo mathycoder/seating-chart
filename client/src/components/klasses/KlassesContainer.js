8ec60cafdb093f93777b511a4b831586dcb84fc0
Adam Sarli <adam.sarli@gmail.com>
2020-02-10 17:35:05 -0500

    styled klasses index into a table

--- client/src/components/klasses/KlassesContainer.js
+++ client/src/components/klasses/KlassesContainer.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 import { fetchKlasses } from '../../actions/klassActions.js'
+import './css/klassesContainer.css'
 
 const KlassesContainer = ({ currentUser, fetchKlasses, klasses }) => {
   useEffect(() => {
@@ -8,20 +9,32 @@ const KlassesContainer = ({ currentUser, fetchKlasses, klasses }) => {
   }, [])
 
   return (
-    <div className="signup-wrapper">
-      <div className="signup-form">
+    <div className="signup-wrapper klass-index-wrapper">
+      <div className="signup-form klass-index">
         <div className="flexseats-title">
           {`${currentUser.firstName}'s Classes`}
+        </div>
+
+        <div className="klass-row">
+          <div><strong>Period</strong></div>
+          <div><strong>Name</strong></div>
+          <div></div>
         </div>
 
         {klasses.allIds.map(klassId => {
           const klass = klasses.byId[klassId]
           return (
             <div className="klass-row">
-              {klass.name}
+              <div>{klass.period}</div>
+              <div>{klass.name}</div>
+              <div>
+                <button className="myButton">Edit</button>
+              </div>
             </div>
           )
         })}
+
+        <button className="myButton">Create Class</button>
 
       </div>
     </div>
