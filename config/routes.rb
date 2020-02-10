d2bc36ed7b460c3165f6711284645f4b23f1a676
Adam Sarli <adam.sarli@gmail.com>
2020-02-09 20:54:00 -0500

    added fallback route for Rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get '*path', to: "static_pages#index", constraints: ->(request) do
+    !request.xhr? && request.format.html?
+  end
+  
   get '/get_current_user', to: 'sessions#get_current_user'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  
+
   resources :teachers, only: [:create]
 end
