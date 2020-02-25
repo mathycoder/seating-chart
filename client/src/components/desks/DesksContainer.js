b4c972408f9514efa4046930d3eb97a6ff40e720
Adam Sarli <adam.sarli@gmail.com>
2020-02-25 11:25:55 -0500

    added react-beautiful-dnd npm and initial <DragDropContext/>

--- client/src/components/desks/DesksContainer.js
+++ client/src/components/desks/DesksContainer.js
@@ -1,20 +1,27 @@
 import React from 'react'
 import Desk from './Desk'
 import './css/desks-container.css'
+import { DragDropContext } from 'react-beautiful-dnd'
 
 const DesksContainer = ({ klass, students }) => {
+  const handleDragEnd = () => {
+    console.log("drag ended!")
+  }
+  
   return (
-    <div className="desks-container">
-      {students.allIds.map((studentId, index) => {
-        const student = students.byId[studentId]
-        return (
-          <>
-            <Desk student={student}/>
-            {index % 2 === 1 ? <div className="gap"></div> : null}
-          </>
-        )
-      })}
-    </div>
+    <DragDropContext onDragEnd={() => handleDragEnd()}>
+      <div className="desks-container">
+        {students.allIds.map((studentId, index) => {
+          const student = students.byId[studentId]
+          return (
+            <>
+              <Desk student={student}/>
+              {index % 2 === 1 ? <div className="gap"></div> : null}
+            </>
+          )
+        })}
+      </div>
+    </DragDropContext>
   )
 }
 
