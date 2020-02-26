c40e6f1268ad2f9ff6d322f83b76bb5629240516
Adam Sarli <adam.sarli@gmail.com>
2020-02-26 16:10:10 -0500

    sticking to css grid for now, even though it's buggy with react-beautiful-dnd

--- client/src/components/desks/GroupDesksContainer.js
+++ client/src/components/desks/GroupDesksContainer.js
@@ -15,7 +15,11 @@ const Group = SortableContainer(({students}) => {
   return (
     <div className="group">
       {students.map((student, index) => (
-        <SortableItem key={`item-${student.id}`} index={index} student={student} />
+        <SortableItem
+          key={`item-${student.id}`}
+          index={index}
+          container={1}
+          student={student} />
       ))}
     </div>
   );
@@ -26,7 +30,15 @@ const GroupDesksContainer = ({ klass, students }) => {
   return (
     <div className="group-seating-wrapper">
       {[...Array(Math.ceil(students.allIds.length / 4)).keys()].map((groupNum, index) => (
-          <Group axis="xy" students={myStudents.slice(index*4,(index+1)*4)} />
+          <Group
+            key={index}
+            axis="xy"
+            pressDelay={200}
+            onSortOver={({index, oldIndex, newIndex, collection, isKeySorting}, e) => {
+
+            }}
+            students={myStudents.slice(index*4,(index+1)*4)}
+          />
       ))}
     </div>
   )
