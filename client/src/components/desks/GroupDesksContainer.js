7722a056d73631b06d6b065c7e9723ad4f3069cc
Adam Sarli <adam.sarli@gmail.com>
2020-02-26 13:37:21 -0500

    basic demo of react-sortable-hoc

--- client/src/components/desks/GroupDesksContainer.js
+++ client/src/components/desks/GroupDesksContainer.js
@@ -1,11 +1,28 @@
 import React from 'react'
+import { SortableContainer, SortableElement } from 'react-sortable-hoc'
 
-const GroupDesksContainer = () => {
+const SortableItem = SortableElement(({value}) => <div>{value}</div>)
+
+const SortableList = SortableContainer(({items}) => {
   return (
     <div>
-      GroupDesksContainer
+      {items.map((value, index) => (
+        <SortableItem key={`item-${value}`} index={index} value={value} />
+      ))}
     </div>
-  )
+  );
+});
+
+const GroupDesksContainer = ({ klass, students }) => {
+
+  const myStudents = () => {
+    return students.allIds.map(stId => {
+      const student = students.byId[stId]
+      return student.firstName
+    })
+  }
+
+  return <SortableList items={myStudents()} />
 }
 
 export default GroupDesksContainer
