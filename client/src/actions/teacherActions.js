2695334e033fdf09a56c7d7cb98709d2ea863bb9
Adam Sarli <adam.sarli@gmail.com>
2020-03-09 19:54:04 -0400

    creating new account redirects to classes page

--- client/src/actions/teacherActions.js
+++ client/src/actions/teacherActions.js
@@ -11,11 +11,11 @@ export function signupTeacher(teacherData, history){
       .then(resp => resp.json())
       .then(user => {
         if (user.error){
-          //dispatch({ type: 'ADD_FLASH_MESSAGE', message: user.error })
+          dispatch({ type: 'ADD_FLASH_MESSAGE', message: user.error })
         } else {
           dispatch({ type: 'SET_CURRENT_USER', user })
-          //dispatch({ type: 'ADD_FLASH_MESSAGE', message: "Created New Teacher Account" })
-          //history.push('/classes')
+          dispatch({ type: 'ADD_FLASH_MESSAGE', message: "Created New Teacher Account" })
+          history.push('/classes')
         }
       })
       .catch(console.log)
