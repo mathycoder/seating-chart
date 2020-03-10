18caa9440f6ce21135078d72a09b1ef3a273749f
Adam Sarli <adam.sarli@gmail.com>
2020-03-10 18:13:06 -0400

    added touch drag <Preview />

--- client/src/components/desks/Desk.js
+++ client/src/components/desks/Desk.js
@@ -3,6 +3,7 @@ import './css/desk.css'
 import { useDrag, useDrop } from 'react-dnd'
 import { swapSeats } from '../../actions/studentActions.js'
 import { connect } from 'react-redux'
+import MultiBackend, { Preview } from 'react-dnd-multi-backend';
 
 const Desk = ({ klass, student, students, index, swap, type }) => {
   const [{ isDragging }, drag] = useDrag({
@@ -22,6 +23,17 @@ const Desk = ({ klass, student, students, index, swap, type }) => {
     },
   })
 
+  const generatePreview = ({itemType, item, style}) => {
+    return (
+      <div className={`desk`}>
+        {student.firstName}
+        <br/>
+        {student.lastName}
+        <br/>
+      </div>
+    )
+  };
+
   return (
     <>
       <div ref={drop}>
@@ -33,6 +45,7 @@ const Desk = ({ klass, student, students, index, swap, type }) => {
           <span className="academic-score">{student.academicScore}</span>
           <span className="behavior-score">{student.behaviorScore}</span>
         </div>
+        <Preview generator={generatePreview} />
       </div>
       {index % 2 === 1 ? <div className="gap"></div> : null}
     </>
