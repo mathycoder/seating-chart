9f41efa081aa892d1be014d19500cff4b017e369
Adam Sarli <adam.sarli@gmail.com>
2020-03-10 12:45:44 -0400

    added mouse listening events to navbar

--- client/src/components/navBar/NavBar.js
+++ client/src/components/navBar/NavBar.js
@@ -1,21 +1,40 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import './navbar.css'
 import { NavLink } from "react-router-dom"
 import { connect } from 'react-redux'
 
 const NavBar = ({ currentUser, klasses }) => {
   const [ klassDropdown, setKlassDropdown ] = useState(false)
+  const refKlassDropdown = useRef();
+
+  const handleClick = (e) => {
+    if (refKlassDropdown.current.contains(e.target)) { return }
+    setKlassDropdown(false)
+  }
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClick)
+    return () => {
+      document.removeEventListener('mousedown', handleClick)
+    }
+  }, [])
+
+  // useEffect(() => {
+  //
+  // }, [])
 
   const title = () => <><strong>Flex</strong>Seats</>
 
   const renderKlassDropdown = () => {
     return (
-      <div className={`dropdown-menu klass-dropdown ${klassDropdown ? 'opened': 'closed'}`}>
+      <div
+        ref={refKlassDropdown}
+        className={`dropdown-menu klass-dropdown ${klassDropdown ? 'opened': 'closed'}`}>
           {klasses.allIds.map((klassId, index) => {
             const klass = klasses.byId[klassId]
             return (
               <NavLink
-                to={`/classes/${klass.id}`}
+                to={`/classes/${klass.id}/pairs`}
                 onClick={() => setKlassDropdown(false)}
                 key={index}
                 >
