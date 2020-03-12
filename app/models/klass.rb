2f733d2d5bbe44b84fa3970fcc9ef4a9695fd53d
Adam Sarli <adam.sarli@gmail.com>
2020-03-12 13:26:02 -0400

    created initial homogenous group algorithm

--- app/models/klass.rb
+++ app/models/klass.rb
@@ -2,6 +2,11 @@ class Klass < ApplicationRecord
   belongs_to :teacher
   has_many :students
 
+  GROUPS = [[0,1,8,9], [2,3,10,11], [4,5,12,13], [6,7,14,15],
+            [16,17,24,25], [18,19,26,27], [20,21,28,29], [22,23,30,31]]
+
+  GROUPS_FLAT = GROUPS.flatten
+
   def assign_seats(student)
     seat_pair = nil
     seat_group = nil
@@ -45,7 +50,23 @@ class Klass < ApplicationRecord
   end
 
   def generate_seats_groups_hetero
-    binding.pry
+    # my goal is to group by averages
+    total_groups = (self.students.length.to_f / 4).ceil
+    sorted_students = self.students.sort_by{|student| student.academic_score + student.behavior_score}.reverse
+    student_data = {}
+    seated_students = []
+    while sorted_students.length > 0
+      seated_students << sorted_students.pop
+      seated_students << sorted_students.shift if sorted_students.length != 0
+    end
+    seated_students.each_with_index{|student, index| student_data[student.id] = {group: index % total_groups} }
+    student_data_array = student_data.sort_by{|stId, hash| hash[:group]}
+    student_array = student_data_array.map{|arr| Student.find_by(id: arr[0])}
+    student_array.each_with_index do |student, index|
+      student[:seat_group] = GROUPS_FLAT[index]
+      student.update(seat_group: GROUPS_FLAT[index])
+    end
+    student_array
   end
 
 end
