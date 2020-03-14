291beb689475c9b5b3e6b96a290b393f8a4e6614
Adam Sarli <adam.sarli@gmail.com>
2020-03-13 23:40:07 -0400

    fixed drag preview error

--- client/src/backends/GeneratePreview.js
+++ client/src/backends/GeneratePreview.js
@@ -1,27 +1,16 @@
 import React from 'react'
-import { connect } from 'react-redux'
 
-const GeneratePreview = ({itemType, item, style,
-                          currentBehavior, currentAcademics}) => {
+const GeneratePreview = ({itemType, item, style}) => {
   const { student } = item
   return (
     <div style={style} id="preview" className={`desk`}>
-      <div className={`${currentAcademics || currentBehavior ? '' : 'groove'}`}/>
+      <div className={`groove`}/>
       <div className="first-name">{student.firstName}</div>
       <div className="last-name">{student.lastName}</div>
-      <div className="ratings">
-        {currentAcademics ? <span className="academic-score">{student.academicScore}</span> : null}
-        {currentBehavior ? <span className="behavior-score">{student.behaviorScore}</span> : null}
-      </div>
     </div>
   )
 }
 
-const mapStateToProps = state => {
-  return {
-    currentBehavior: state.currentKlass.behavior,
-    currentAcademics: state.currentKlass.academics
-  }
-}
 
-export default connect(mapStateToProps, null)(GeneratePreview)
+
+export default GeneratePreview
