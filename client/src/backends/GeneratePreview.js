562bbf4ce521aafacc8ff0a8fa5b9bf7aaf7ee7b
Adam Sarli <adam.sarli@gmail.com>
2020-03-13 23:32:20 -0400

    preview should match current desk format

--- client/src/backends/GeneratePreview.js
+++ client/src/backends/GeneratePreview.js
@@ -1,12 +1,27 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
-const GeneratePreview = ({itemType, item, style}) => {
+const GeneratePreview = ({itemType, item, style,
+                          currentBehavior, currentAcademics}) => {
   const { student } = item
   return (
     <div style={style} id="preview" className={`desk`}>
+      <div className={`${currentAcademics || currentBehavior ? '' : 'groove'}`}/>
       <div className="first-name">{student.firstName}</div>
+      <div className="last-name">{student.lastName}</div>
+      <div className="ratings">
+        {currentAcademics ? <span className="academic-score">{student.academicScore}</span> : null}
+        {currentBehavior ? <span className="behavior-score">{student.behaviorScore}</span> : null}
+      </div>
     </div>
   )
 }
 
-export default GeneratePreview
+const mapStateToProps = state => {
+  return {
+    currentBehavior: state.currentKlass.behavior,
+    currentAcademics: state.currentKlass.academics
+  }
+}
+
+export default connect(mapStateToProps, null)(GeneratePreview)
