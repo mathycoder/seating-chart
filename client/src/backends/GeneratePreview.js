ebbdb67d1afe3f08565c4c549961e2581189e01e
Adam Sarli <adam.sarli@gmail.com>
2020-03-13 23:24:09 -0400

    formatting preview drag desk for mobile

--- client/src/backends/GeneratePreview.js
+++ client/src/backends/GeneratePreview.js
@@ -3,13 +3,8 @@ import React from 'react'
 const GeneratePreview = ({itemType, item, style}) => {
   const { student } = item
   return (
-    <div style={style} className={`desk`}>
+    <div style={style} id="preview" className={`desk`}>
       <div className="first-name">{student.firstName}</div>
-      <div className="last-name">{student.lastName}</div>
-      <div className="ratings">
-        <span className="academic-score">{student.academicScore}</span>
-        <span className="behavior-score">{student.behaviorScore}</span>
-      </div>
     </div>
   )
 }
